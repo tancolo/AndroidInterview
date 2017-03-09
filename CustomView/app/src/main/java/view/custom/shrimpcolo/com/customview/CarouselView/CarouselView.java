d76a7d4b2e3c6c7c564b6ed9cca8cb1e02cbbed5
JohnnyTam <shrimpcolo@gmail.com>
2017-03-09 09:51:28 +0800

    添加rxjava实现动画Demo

---
+++ CustomView/app/src/main/java/view/custom/shrimpcolo/com/customview/CarouselView/CarouselView.java
@@ -0,0 +1,8 @@
+package view.custom.shrimpcolo.com.customview.CarouselView;
+
+/**
+ * Created by Johnny Tam on 2017/3/6.
+ */
+
+public class CarouselView {
+}
---
+++ CustomView/app/src/main/java/view/custom/shrimpcolo/com/customview/RxjavaAnimation/RxjavaAnimation.java
@@ -0,0 +1,15 @@
+package view.custom.shrimpcolo.com.customview.RxjavaAnimation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import view.custom.shrimpcolo.com.customview.R;
+
+public class RxjavaAnimation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rxjava_animation);
+    }
+}
