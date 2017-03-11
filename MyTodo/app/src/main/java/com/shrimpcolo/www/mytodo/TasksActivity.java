f9d77492f08d0c5aed5230df0ab25c78315ab259
JohnnyTam <shrimpcolo@gmail.com>
2017-03-11 16:50:21 +0800

    添加TaskActivity中对应的菜单图标

--- MyTodo/app/src/main/java/com/shrimpcolo/www/mytodo/TasksActivity.java
+++ MyTodo/app/src/main/java/com/shrimpcolo/www/mytodo/TasksActivity.java
@@ -23,7 +23,7 @@ public class TasksActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_add_task);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
