710c1a9a66e643418f570c536a890ec62db9d7fe
JohnnyTam <shrimpcolo@gmail.com>
2017-04-05 12:32:50 +0800

    使用facebook stetho 查看realm数据库, 同样适用于sqlite

--- TodoKotlin/app/src/main/java/com/shrimpcolo/www/todokotlin/TaskApplication.kt
+++ TodoKotlin/app/src/main/java/com/shrimpcolo/www/todokotlin/TaskApplication.kt
@@ -1,6 +1,8 @@
 package com.shrimpcolo.www.todokotlin
 
 import android.app.Application
+import com.facebook.stetho.Stetho
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider
 import io.realm.Realm
 
 /**
@@ -10,5 +12,11 @@ import io.realm.Realm
 class TaskApplication : Application() {
     override fun onCreate() {
         Realm.init(this)
+
+        Stetho.initialize(
+                Stetho.newInitializerBuilder(this)
+                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                        .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
+                        .build())
     }
 }
